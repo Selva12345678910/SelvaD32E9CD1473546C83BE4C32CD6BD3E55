0e86295fe0fabd0cf8d1740efe5f1fc3a9f5ff48
Selva12345678910 <144313526+Selva12345678910@users.noreply.github.com>
2023-10-15 12:39:43 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,39 +1,19 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            return f"Deposited ${amount}. New balance: ${self.__account_balance}"
-        else:
-            return "Invalid deposit amount. Amount must be greater than zero."
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
-            return f"Withdrew ${amount}. New balance: ${self.__account_balance}"
-        elif amount > self.__account_balance:
-            return "Insufficient funds."
-        else:
-            return "Invalid withdrawal amount. Amount must be greater than zero."
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
 
-    def display_balance(self):
-        return f"Account balance for {self.__account_holder_name}: ${self.__account_balance}"
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
 
-
-# Example usage:
-if __name__ == "__main__":
-    # Create a BankAccount instance
-    account = BankAccount("123456789", "John Doe", 1000.0)
-
-    # Deposit money
-    print(account.deposit(500))  # Deposited $500. New balance: $1500.0
-
-    # Withdraw money
-    print(account.withdraw(200))  # Withdrew $200. New balance: $1300.0
-
-    # Display account balance
-    print(account.display_balance())  # Account balance for John Doe: $1300.0
+# Call the play() method for each object
+batsman.play()  # Output: The batsman is batting.
+bowler.play()   # Output: The bowler is bowling.
